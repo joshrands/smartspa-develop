26f838c85a0ec2376c012e0d3d138a5ceb94ca91
Josh Rands <jmrands@mymail.mines.edu>
2020-01-03 11:26:24 -0700

    More clowning around with linear ph scale

--- ph-test.py
+++ ph-test.py
@@ -2,9 +2,12 @@ import cv2
 import numpy as np
 print(cv2.__version__)
 
+ph_start = 6.4
+ph_end = 7.2
+
 # classify each ph reading as RGB value  
 
-img = cv2.imread('pH-colors/6_2.png')
+img = cv2.imread('pH-colors/6_4.png')
 
 total_blue = 0
 total_green = 0
@@ -23,7 +26,7 @@ min_red = total_red / (len(img) * len(img))
 
 print(min_red, min_green, min_blue)
 
-img = cv2.imread('pH-colors/7_6.png')
+img = cv2.imread('pH-colors/7_2.png')
 
 total_blue = 0
 total_green = 0
@@ -42,25 +45,25 @@ max_red = total_red / (len(img) * len(img))
 
 print(max_red, max_green, max_blue)
 
-img_name = "7_6.png"# raw_input("Enter new image path or 'q' to quit: ")
+img_name = "6_8.png"# raw_input("Enter new image path or 'q' to quit: ")
 
 delta_red = float(max_red - min_red)
 delta_green = float(max_green - min_green)
 delta_blue = float(max_blue - min_blue)
 print("Deltas", delta_red, delta_green, delta_blue)
-total_delta = abs(delta_red) + abs(delta_green) + abs(delta_blue)
+total_delta = (delta_red) + (delta_green) + (delta_blue)
 
 # weigh each color from delta 
-red_weight = float(abs(delta_red) / total_delta)
-green_weight = float(abs(delta_green) / total_delta)
-blue_weight = float(abs(delta_blue) / total_delta)
+red_weight = abs(float(delta_red / total_delta))
+green_weight = abs(float(delta_green / total_delta))
+blue_weight = abs(float(delta_blue / total_delta))
 print(red_weight, green_weight, blue_weight)
 
-ph_start = 6.2
-ph_end = 7.6
 ph_range = ph_end - ph_start
 
 while (img_name != "q"):
+    print(img_name)
+
     img = cv2.imread("pH-colors/" + img_name)
 
     total_blue = 0
@@ -81,14 +84,14 @@ while (img_name != "q"):
     print(red, green, blue)
 
     # classify the pH
-    dif_red = max_red - red
-    dif_green = max_green - green
-    dif_blue = max_blue - blue
-    print(dif_red, dif_green, dif_blue)
+    dif_red = (max_red - red)
+    dif_green = (max_green - green)
+    dif_blue = (max_blue - blue)
+    print("Diffs", dif_red, dif_green, dif_blue)
 
     delta_ph = dif_red * red_weight + dif_green * green_weight + dif_blue * blue_weight
-    print("pH = " + str(ph_start + delta_ph))
+    print("pH = " + str(ph_end + delta_ph))
 
-    img_name = input("Enter new image path or 'q' to quit: ")
+    img_name = raw_input("Enter new image path or 'q' to quit: ")
 
 
