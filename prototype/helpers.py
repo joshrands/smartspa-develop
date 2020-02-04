b72ef9344cfa45f1fb621c2f662ed4777d97ee66
Josh Rands <jmrands@mymail.mines.edu>
2020-02-03 20:22:48 -0700

    Added xml parsing capabilities for config file.

---
+++ prototype/helpers.py
@@ -0,0 +1,33 @@
+"""@package helpers
+Python helper functions for performing tedious work.
+ 
+Author: Josh Rands
+Date: 2/3/2020 
+Email: joshrands1@gmail.com
+"""
+
+import xml.etree.ElementTree as ET
+import logging as log
+
+
+def parse_xml(file_name):
+	"""Parse an xml file into a python dictionary
+	"""
+
+	root = ET.parse(file_name).getroot()
+
+	data = {}
+
+	count = 0
+	for child in root:
+
+		if (root[count].text == 'true' or root[count].text == 'false'):
+			data[child.tag] = bool(root[count].text)	
+		else:	
+			data[child.tag] = root[count].text	
+	
+		count += 1
+
+	return data
+
+
---
+++ prototype/sensing.py
@@ -0,0 +1,13 @@
+"""@package sensing
+Perform SmartSpa chemical level sensing 
+ 
+This module performs chemical level sensing by releasing a reagent into the mixing container,
+driving a stirring motor 
+ 
+Author: Josh Rands
+Date: 2/3/2020 
+Email: joshrands1@gmail.com
+"""
+
+
+
