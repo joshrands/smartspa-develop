aa07a8d5f6da15c01d410bc617934501ab952089
Josh Rands <jmrands@mymail.mines.edu>
2020-02-07 20:31:54 -0700

    Changed php to execute full python3 command to work on rpi

--- setup/setup_network.php
+++ setup/setup_network.php
@@ -6,8 +6,8 @@ $wifi_network = $_POST['wifiNetwork'];
 $wifi_password = $_POST['wifiPassword'];
 
 /* Run python script to setup network... */ 
-$command = escapeshellcmd('./setup_network.py ' . $wifi_network . ' ' . $wifi_password);
-$output = shell_exec($command);
+//$command = escapeshellcmd('./setup_network.py ' . $wifi_network . ' ' . $wifi_password);
+$output = shell_exec('python3 setup_network.py ' . $wifi_network . ' ' . $wifi_password);
 echo $output;
 
 echo "<br>";
