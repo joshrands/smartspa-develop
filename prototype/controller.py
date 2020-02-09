2b1b59309aac3edfbf51ff1af34ceabe85623dc9
Josh Rands <jmrands@mymail.mines.edu>
2020-02-09 14:01:54 -0700

    Added release_chemical function skeleton and added more to controller (turn off jets and main line pump)

--- prototype/controller.py
+++ prototype/controller.py
@@ -7,7 +7,15 @@ Email: joshrands1@gmail.com
 """
 
 import logging as log
+import time
+
 from sensing import get_error
+from release_chemical import release_chemical 
+import init
+
+# Global params
+spa_mix_time_s = 5
+
 
 def balance_chemical(chemical, vis=False):
 	"""Balance the given chemical
@@ -15,7 +23,21 @@ def balance_chemical(chemical, vis=False):
 
 	log.info("Balancing %s" % chemical)
 
+	# get error from sensing system
 	error = get_error(chemical, vis)
 
 	log.info("%s error = %.4d" % (chemical, error))
+
+	# release appropriate amount of chemical into system
+	release_chemical(chemical, 0)
+
+	# let jets and main line pump mix chemicals into spa 
+	log.info("Mixing chemicals for %d seconds..." % spa_mix_time_s)
+	time.sleep(spa_mix_time_s)
+
+	# Turn off spa jets 
+	init.hardware.set_pin('spa_jets', False)
+
+	# Turn off main line pump
+	init.hardware.set_pin('main_line_pump', False)
 
--- prototype/release_chemical.py
+++ prototype/release_chemical.py
@@ -9,3 +9,13 @@ Email: joshrands1@gmail.com
 import logging as log
 
 
+def release_chemical(chemical, quantity_grams):
+    log.info("Releasing %d grams of %s" % (quantity_grams, chemical))
+    log.warning("Chemical release system not implemented.")
+
+    # TODO: Release chemical
+
+    # Done! 
+
+
+
