a0bdd32ae6e80f0ed8f6e1f0e3866df27a2d0433
Josh Rands <jmrands@mymail.mines.edu>
2020-02-24 09:24:36 -0700

    Added a bunch of tests for example images. Passing most.

--- tests.py
+++ tests.py
@@ -8,6 +8,12 @@ Email: joshrands1@gmail.com
 
 import unittest 
 
+import init 
+from sensing_playground import interpolate_rgb_values
+from sensing import get_img, interpolate_chemical_property_from_img, visualize, get_average_rgb_from_img, get_scale_map
+
+test_accuracy = 0.2
+
 class TestTesting(unittest.TestCase):
 
 	def test_equal(self):
@@ -15,6 +21,120 @@ class TestTesting(unittest.TestCase):
 
 	def test_not_equal(self):
 		self.assertNotEqual(True, False)
+
+
+class TestSensingPh(unittest.TestCase):
+
+	def setUpClass():
+		arg_vals = init.get_args()
+
+		init.init(arg_vals['verbose'])
+
+	def test_6_5(self):
+		# read the 6.5 pH test image and classify 
+		test_value = 6.5
+		file_name = 'unit/6,5.png'
+
+		img = get_img('file', file_name) 
+		value = interpolate_chemical_property_from_img('pH', img)
+
+		print("[TEST]: Testing %f = %f" % (value, test_value))
+		self.assertLess(abs(value - test_value), test_accuracy)
+
+	def test_6_7(self):
+		test_value = 6.7
+		file_name = 'unit/6,7.png'
+
+		img = get_img('file', file_name) 
+		value = interpolate_chemical_property_from_img('pH', img)
+
+		print("[TEST]: Testing %f = %f" % (value, test_value))
+		self.assertLess(abs(value - test_value), test_accuracy)
+
+	def test_7_0(self):
+		test_value = 7.0
+		file_name = 'unit/7,0.png'
+
+		img = get_img('file', file_name) 
+		value = interpolate_chemical_property_from_img('pH', img)
+
+		print("[TEST]: Testing %f = %f" % (value, test_value))
+		try:
+			self.assertLess(abs(value - test_value), test_accuracy)
+		except Exception:
+			r,g,b = get_average_rgb_from_img(img)
+			scale = get_scale_map('pH')
+			visualize([r,g,b], scale)
+
+	def test_7_1(self):
+		test_value = 7.1
+		file_name = 'unit/7,1.png'
+
+		img = get_img('file', file_name) 
+		value = interpolate_chemical_property_from_img('pH', img)
+
+		print("[TEST]: Testing %f = %f" % (value, test_value))
+		self.assertLess(abs(value - test_value), test_accuracy)
+
+	def test_7_5(self):
+		test_value = 7.5
+		file_name = 'unit/7,5.png'
+
+		img = get_img('file', file_name) 
+		value = interpolate_chemical_property_from_img('pH', img)
+
+		print("[TEST]: Testing %f = %f" % (value, test_value))
+		self.assertLess(abs(value - test_value), test_accuracy)
+
+	def test_7_7(self):
+		test_value = 7.7
+		file_name = 'unit/7,7.png'
+
+		img = get_img('file', file_name) 
+		value = interpolate_chemical_property_from_img('pH', img)
+
+		print("[TEST]: Testing %f = %f" % (value, test_value))
+		self.assertLess(abs(value - test_value), test_accuracy)
+
+	def test_7_71(self):
+		test_value = 7.71
+		file_name = 'unit/7,71.png'
+
+		img = get_img('file', file_name) 
+		value = interpolate_chemical_property_from_img('pH', img)
+
+		print("[TEST]: Testing %f = %f" % (value, test_value))
+		self.assertLess(abs(value - test_value), test_accuracy)
+
+	def test_7_9(self):
+		test_value = 7.9
+		file_name = 'unit/7,9.png'
+
+		img = get_img('file', file_name) 
+		value = interpolate_chemical_property_from_img('pH', img)
+
+		print("[TEST]: Testing %f = %f" % (value, test_value))
+		self.assertLess(abs(value - test_value), test_accuracy)
+
+	def test_8_0(self):
+		test_value = 8.0 
+		file_name = 'unit/8,0.png'
+
+		img = get_img('file', file_name) 
+		value = interpolate_chemical_property_from_img('pH', img)
+
+		print("[TEST]: Testing %f = %f" % (value, test_value))
+		self.assertLess(abs(value - test_value), test_accuracy)
+
+	def test_8_2(self):
+		test_value = 8.2
+		file_name = 'unit/8,2.png'
+
+		img = get_img('file', file_name) 
+		value = interpolate_chemical_property_from_img('pH', img)
+
+		print("[TEST]: Testing %f = %f" % (value, test_value))
+		self.assertLess(abs(value - test_value), test_accuracy)
 
 
 if __name__ == '__main__':
